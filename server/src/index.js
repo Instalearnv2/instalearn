82b67b66d4df025c2af537b1988d689f9092fe89
hibernuts <raghavjindal0212@gmail.com>
2025-04-30 23:39:43 +0530

    .

--- server/src/index.js
+++ server/src/index.js
@@ -13,7 +13,12 @@ const port = process.env.PORT || 8082;
 
 app.use(compression());
 app.use(helmet());
-app.use(cors("*"));
+app.use(
+  cors({
+    origin: "https://ai.decentraclasses.com", // or "*" for all origins (not recommended for production)
+    credentials: true, // optional: allow cookies/auth headers
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
